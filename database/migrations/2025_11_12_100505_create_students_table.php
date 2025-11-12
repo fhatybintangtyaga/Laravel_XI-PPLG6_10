4ab9210266ea7c2a4641fd8fa472f87e40f3aa2e
Aziz Fandika Pamungkas <20102293@ittelkom-pwt.ac.id>
2025-11-12 17:08:15 +0700

    Add Feature Read Student

--- database/migrations/2025_11_12_100505_create_students_table.php
+++ database/migrations/2025_11_12_100505_create_students_table.php
@@ -13,10 +13,6 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('nis')->unique();
-            $table->string('nama_lengkap');
-            $table->string('jenis_kelamin', ['L', 'P']);
-            $table->string('nisn')->nullable();
             $table->timestamps();
         });
     }
